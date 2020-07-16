81542e9ae5bc9eff8bb74cba2c15a2931a1d9e79
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2020-07-16 22:09:56 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+	/* Zadacha 6
+	int years;
+	std::cin >> years;
+
+	int days = years * 365;
+	std::cout << "Days: " << days << std::endl;
+
+	int hours = days * 24;
+	std::cout << "Hours: " << hours << std::endl;
+
+	int minutes = hours * 60;
+	std::cout << "Minutes: " << minutes << std::endl;
+
+		return 0;
+		*/
+
+
+}
